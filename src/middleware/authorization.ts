7ad14240bf421b2a6b80a9622883b5e83802ef68
Christo Digiloudis <christodigiloudis@192.168.1.21>
2026-06-25 15:32:52 +0300

    page permissions fix

--- src/middleware/authorization.ts
+++ src/middleware/authorization.ts
@@ -1,20 +1,30 @@
 import { NextResponse } from "next/server";
 
-// types
 import type { NextRequest } from "next/server";
 import type { UserRole } from "@/app/dashboard/actions";
 
-// config
 const routes: Record<string, Array<UserRole>> = {
+	"/dashboard/warehouses/create": [1],
 	"/dashboard/products/create": [1, 2],
+	"/dashboard/products/[id]/edit": [1, 2],
 };
 
 export function authorize(request: NextRequest) {
 	const pathname: string = request.nextUrl.pathname;
 	const role: number = Number(request.cookies.get("role")?.value);
 
-	if (pathname in routes) {
-		const roles: Array<UserRole> = routes[pathname];
+	let matchedRoute: string | null = null;
+
+	for (const route in routes) {
+		const routeRegex = new RegExp("^" + route.replace(/\[.*?\]/g, "[^/]+") + "$");
+		if (routeRegex.test(pathname)) {
+			matchedRoute = route;
+			break;
+		}
+	}
+
+	if (matchedRoute) {
+		const roles: Array<UserRole> = routes[matchedRoute];
 		if (isNaN(role) || !roles.includes(role as UserRole)) {
 			const referenceUrl = request.headers.get("referer");
 			if (referenceUrl) return NextResponse.redirect(referenceUrl);
